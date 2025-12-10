8d9ca334c57067cfe7b06614baf69f5187515bcd
Ozias Costa <oziasmcn@gmail.com>
2025-12-10 16:25:03 -0300

    Adição de pasta

---
+++ backend/src/services/EducaOnline.Pedidos.API/Configurations/ApiConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using EducaOnline.WebAPI.Core.Identidade;
+using EducaOnLine.Pedidos.API.Data;
+using MeuProjeto.WebAPI.Core.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducaOnline.Pedidos.API.Configurations
+{
+    public static class ApiConfiguration
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PedidosContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            services.AddControllers();
+            
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total",
+                    builder =>
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+
+            services.AddHealthCheckConfig(configuration);
+
+            return services;
+        }
+
+        public static WebApplication UseApiConfiguration(this WebApplication app)
+        {
+
+            if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Docker"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("Total");
+
+            app.UseAuthConfiguration();
+
+            app.UseHealthCheckConfig();
+
+            app.MapControllers();            
+
+            return app;
+        }
+    }
+
+
+}
